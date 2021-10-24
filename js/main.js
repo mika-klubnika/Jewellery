a44ec4a290f560f72c66f9a90e20b1ad470079bc
Марина <mika-klubnika@yandex.ru>
2021-10-24 21:19:00 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+'use strict';
+//аккордеон
+const accordion = document.querySelector('.accordion');
+const accordionButton = accordion.querySelectorAll('.accordion__button');
+const accordionItems = accordion.querySelectorAll('.accordion__item');
+
+if(accordion && accordionButton && accordionItems) {
+  accordionItems.forEach(item => {
+    item.classList.remove('accordion__item--nojs');
+  })
+
+  function toggleAccordion () {
+    const thisItem = this.parentNode;
+
+    accordionItems.forEach(item => {
+      if (thisItem == item) {
+        thisItem.classList.toggle('accordion__item--active');
+        return;
+      }
+      item.classList.remove('accordion__item--active');
+    });
+  }
+
+  accordionButton.forEach(block => block.addEventListener('click', toggleAccordion));
+}
+
+'use strict';
 //МЕНЮ
 const header = document.querySelector('.header');
 const navigation = header.querySelector('.navigation');
