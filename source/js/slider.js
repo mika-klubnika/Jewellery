251af965e90cb5ebac758f65bf48ff6833555ff7
Марина <mika-klubnika@yandex.ru>
2021-10-25 16:40:40 +0300

    slider

---
+++ source/js/slider.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  slidesPerGroup: 2,
+  slidesPerView: 2,
+  centeredSlides: false,
+  centeredSlidesBounds: true,
+
+  // If we need pagination
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+
+  // // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // }
+});
