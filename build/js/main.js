82976dcacbcc0ad0dd1f3a0c6bfd963e2c89ac47
Марина <mika-klubnika@yandex.ru>
2021-10-29 00:53:31 +0300

    slider

--- build/js/main.js
+++ build/js/main.js
@@ -156,23 +156,96 @@ if (header && toggle && navigation) {
   });
 };
 
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   loop: true,
-//   slidesPerGroup: 2,
-//   slidesPerView: 2,
-//   centeredSlides: false,
-//   centeredSlidesBounds: true,
+if (document.querySelector('.swiper-pagination') &&
+  document.querySelector('.swiper-mobile-pagination__current') &&
+  document.querySelector('.swiper-mobile-pagination__total')) {
 
-//   // If we need pagination
-//   // pagination: {
-//   //   el: ".swiper-pagination",
-//   //   clickable: true,
-//   // },
+  let swiper;
+  const swiperPaginationBlock = document.querySelector('.swiper-pagination');
+  const current = document.querySelector('.swiper-mobile-pagination__current');
+  const total = document.querySelector('.swiper-mobile-pagination__total');
 
-//   // // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   }
-// });
+  const ACTIVE_BULLET = 'swiper-pagination-bullet-active';
+  const BREAKPOINT_MOBILE = 767;
+
+  const initSwiper = () => {
+    swiper = new Swiper(".swiper", {
+      loop: true,
+      slidesPerGroup: 4,
+      slidesPerView: 4,
+      centeredSlides: false,
+      centeredSlidesBounds: true,
+
+      pagination: {
+        el: '.slider__pagination--desktop',
+        clickable: 'true',
+        renderBullet(index, className) {
+          return '<button class="' + className + '">' + (index + 1) + '</button>';
+        },
+      },
+
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+      breakpoints: {
+        320: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+        },
+        1023: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+        },
+        1169: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+        },
+      },
+    });
+  };
+
+  const getCurrentBullet = (bullets) => {
+    let currentBullet;
+    Array.from(bullets).forEach((element) => {
+      if (element.classList.contains(ACTIVE_BULLET)) {
+        currentBullet = +element.textContent;
+      }
+    });
+
+    return currentBullet;
+  }
+
+  const renderMobilePagination = (bullets) => {
+    total.textContent = bullets.length;
+    current.textContent = getCurrentBullet(bullets);
+  }
+
+  const changeIndex = (bullets) => {
+    swiper.on('transitionEnd', () => {
+      renderMobilePagination(bullets);
+    });
+  }
+
+  const changeBreakpoint = (bullets) => {
+    let viewport = document.documentElement.clientWidth;
+
+    if (viewport < BREAKPOINT_MOBILE) {
+      changeIndex(bullets);
+    }
+  }
+
+  const initSlider = () => {
+    initSwiper();
+
+    if (swiper && swiperPaginationBlock && current && total) {
+      let bullets = swiperPaginationBlock.children;
+
+      renderMobilePagination(bullets);
+      changeBreakpoint(bullets);
+    }
+  }
+
+  initSlider();
+};
