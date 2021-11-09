ceb3253f84e9c8eca2725da1f5583bee4b5c0a34
Марина <mika-klubnika@yandex.ru>
2021-11-09 19:22:13 +0300

    edits

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,109 +1,82 @@
-if (document.querySelector('.swiper-pagination') &&
-  document.querySelector('.swiper-mobile-pagination__current') &&
-  document.querySelector('.swiper-mobile-pagination__total')) {
+'use strict';
 
-  let swiper;
-  const swiperPaginationBlock = document.querySelector('.swiper-pagination');
-  const current = document.querySelector('.swiper-mobile-pagination__current');
-  const total = document.querySelector('.swiper-mobile-pagination__total');
-
-  const ACTIVE_BULLET = 'swiper-pagination-bullet-active';
-  const BREAKPOINT_MOBILE = 767;
-
-  const initSwiper = () => {
-    swiper = new Swiper(".swiper", {
-      spaceBetween: 30,
-      slidesPerView: 4,
-      centeredSlides: false,
-      centeredSlidesBounds: true,
-
-      pagination: {
-        el: '.slider__pagination--desktop',
-        clickable: 'true',
-        renderBullet(index, className) {
-          return '<button class="' + className + '">' + (index + 1) + '</button>';
+if (document.querySelector('.slider')) {
+  const slider = new Swiper('.swiper', {
+    navigation: {
+      nextEl: '.slider__button--next',
+      prevEl: '.slider__button--prev',
+    },
+    pagination: {
+      el: '.slider__pagination',
+      renderBullet(index, bulletClass) {
+        return `<button class="` + bulletClass + `"type="button">` + (index + 1) + `</button>`;
+      },
+      bulletClass: 'pagination__item',
+      bulletActiveClass: 'pagination__current-page',
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        pagination: {
+          type: 'fraction',
+          renderFraction(currentClass, totalClass, index, total) {
+            return `<span class="` + currentClass + `"type="button">0 ` + index + ` </span>` +
+              `&nbsp;of&nbsp;` + `<span class="` + totalClass + `"type="button">0 ` + total + ` </span>`;
+          },
         },
       },
-
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-
-      breakpoints: {
-        320: {
-          slidesPerView: 2,
-          slidesPerGroup: 2,
-        },
-        1022: {
-          slidesPerView: 4,
-          slidesPerGroup: 4,
+      767: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        pagination: {
+          type: 'bullets',
         },
       },
-    });
-  };
+      1024: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        pagination: {
+          type: 'bullets',
+        },
+      },
+    },
+    lazy: {
+      loadPrevNext: true,
+    },
+    spaceBetween: 30,
+    speed: 1000,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+    watchSlidesVisibility: true,
+  });
 
-  const getBullets = () => {
-    let bullets;
-    if(swiperPaginationBlock) {
-      bullets = swiperPaginationBlock.children;
-    }
-    return bullets
-  }
+  slider.on('progress', () => {
+    inertNotVisible();
+  });
 
-  const setMobileTotalBullet = (bullets) => {
-    let totalBullets = bullets.length;
-    return totalBullets;
-  }
-
-  const getCurrentBullet = (bullets) => {
-    let currentBullet;
-    Array.from(bullets).forEach((element) => {
-      if (element.classList.contains(ACTIVE_BULLET)) {
-        currentBullet = +element.textContent;
+  const inertNotVisible = () => {
+    slider.slides.forEach((slide) => {
+      if (!slide.classList.contains('swiper-slide-visible')) {
+        slide.childNodes[1].setAttribute('tabindex', '-1');
+      } else {
+        slide.childNodes[1].setAttribute('tabindex', '0');
       }
     });
-    return currentBullet;
-  }
-
-  const renderMobilePagination = (bullets) => {
-    total.textContent = setMobileTotalBullet(bullets);
-    current.textContent = getCurrentBullet(bullets);
-  }
-
-  const changeIndex = (bullets) => {
-    swiper.on('transitionEnd', () => {
-      renderMobilePagination(bullets);
-    });
-  }
-
-  const setMobilePagination = () => {
-    let bullets = getBullets();
-    changeIndex(bullets);
-  }
-
-  const changeBreakpoint = () => {
-    let viewport = document.documentElement.clientWidth;
-
-    if (viewport < BREAKPOINT_MOBILE) {
-      setMobilePagination()
+    if (slider.pagination.bullets) {
+      slider.pagination.bullets.forEach((bullet) => {
+        if (bullet.classList.contains('pagination__current-page')) {
+          bullet.setAttribute('tabIndex', '-1');
+        } else {
+          bullet.setAttribute('tabIndex', '0');
+        }
+      });
     }
-  }
+  };
 
-  const initMobilePagination = () => {
-    let bullets = getBullets();
-    renderMobilePagination(bullets);
-  }
-
-  const initSlider = () => {
-    initSwiper();
-
-    if (swiper && swiperPaginationBlock && current && total) {
-      initMobilePagination();
-      changeBreakpoint();
-      swiper.on('breakpoint', changeBreakpoint);
-    }
-  }
-
-  initSlider();
-};
+  setTimeout(inertNotVisible, 0);
+}
